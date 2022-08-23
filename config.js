6d0de37806a1eac46baa20ce61ca86cf16aa0442
NGUYEN KHANH <khanh.nguyen3@monstarlab.com>
2022-08-23 10:16:30 +0700

    update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl =
-  !process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "development"
     ? "https://landing-page-theta-ten.vercel.app/api"
-    : "http://localhost:3000/api";
+    : "https://landing-page-theta-ten.vercel.app/api";
 
 export { apiUrl };
