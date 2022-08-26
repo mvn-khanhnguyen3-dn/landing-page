1c31e50c1576203ff47791eb4d64219d7d0a5c3a
NGUYEN KHANH <khanh.nguyen3@monstarlab.com>
2022-08-26 14:05:24 +0700

    update-head

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Landing page" />
         <link rel="icon" href="/images/favicon.png" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="Landing page" />
+        <meta property="og:image" content="Classified Ads" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="keywords"
+          content="ads, advertise, advertising, classified, classified ads, database, directory, jobs, listing, local, portal"
+        />
+        <meta
+          name="description"
+          content="Choose from 72 Premium classified ads Templates from the #1 source for classified ads Templates. Created by our Global Community of independent Web Developers."
+        />
+        <meta property="og:image" content="/images/2.png" />
       </Head>
       <body>
         <Main />
