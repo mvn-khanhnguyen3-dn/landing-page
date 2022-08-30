9f8c253c7fccb2e6e75bf2e5c96d10a5c14c0797
NGUYEN KHANH <khanh.nguyen3@monstarlab.com>
2022-08-30 16:52:33 +0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,23 @@ import "../styles/styles.scss";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 import Loading from "../components/modules/Loading";
+import Head from "next/head";
 
 const DynamicHeader = dynamic(() => import("@/components/Layout"), {
   suspense: true,
 });
 function MyApp({ Component, pageProps }) {
   return (
-    <Suspense fallback={<Loading />}>
-      <DynamicHeader>
-        <Component {...pageProps} />
-      </DynamicHeader>
-    </Suspense>
+    <>
+      <Head>
+        <title>Classified Ads</title>
+      </Head>
+      <Suspense fallback={<Loading />}>
+        <DynamicHeader>
+          <Component {...pageProps} />
+        </DynamicHeader>
+      </Suspense>
+    </>
   );
 }
 
