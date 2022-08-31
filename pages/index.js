f8d544123f6aefcb8b875168febe3cf8413600d1
NGUYEN KHANH <khanh.nguyen3@monstarlab.com>
2022-08-31 08:24:57 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import SectionCategory from "@/components/Layout/SectionCategory";
 import SectionFeature from "@/components/Layout/SectionFeature";
 import SectionHero from "@/components/Layout/SectionHero";
 import SectionPrice from "@/components/Layout/SectionPrice";
-import { featureData } from "../utils/data";
+import { featureData } from "@/utils/data";
 
 export default function Home() {
   return (
